e36c89dc384f9d673f87cd0a1010e805e4f441ec
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-20 09:26:49 +0200

    feat: Removed include from header file

--- header/PCANBasic.h
+++ header/PCANBasic.h
@@ -19,34 +19,6 @@
 #ifndef __PCANBASICH__
 #define __PCANBASICH__
 
-#include <stdint.h>
-#ifndef DWORD
-#define DWORD uint32_t
-#endif
-
-#ifndef WORD
-#define WORD uint16_t
-#endif
-
-#ifndef UINT64
-#define UINT64 uint64_t
-#endif
-
-#ifndef LPSTR
-#define LPSTR char*
-#endif
-
-#ifndef BYTE
-#define BYTE uint8_t
-#endif
-
-#ifndef __stdcall
-#define __stdcall
-#endif 
-
-#define PCAN_CHANNEL_ILLEGAL PCAN_CHANNEL_UNAVAILABLE
-
-
 ////////////////////////////////////////////////////////////
 // Value definitions
 ////////////////////////////////////////////////////////////
