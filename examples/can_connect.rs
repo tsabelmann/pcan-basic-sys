47a7e7640f3cf1c4dce32b5fbd433bd8d63a7da8
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-20 09:21:44 +0200

    refact: Removed unnecessary import

--- examples/can_connect.rs
+++ examples/can_connect.rs
@@ -2,5 +2,5 @@ extern crate pcan_basic_sys;
 use pcan_basic_sys as pcan;
 
 fn main() {
-   println!("{:?}", unsafe {pcan::CAN_Initialize(1, 0, 0, 0, 0)});
+    println!("{:?}", unsafe { pcan::CAN_Initialize(1, 0, 0, 0, 0) });
 }
