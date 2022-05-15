f3224795a456ccea720a02f3d05c0322f866108d
tsabelmann <tim.sabelmann@hotmail.de>
2022-05-15 13:26:10 +0200

    feat: Updated build.rs to support the Linux platform

--- build.rs
+++ build.rs
@@ -4,18 +4,20 @@ fn main() {
     // use the following target
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    // link against correct target
-    if cfg!(windows) {
-        if cfg!(target_arch = "x86") {
-            println!("cargo:rustc-link-search=native={}/libs/x86/", manifest_dir);
-        } else if cfg!(target_arch = "x86_64") {
-            println!(
-                "cargo:rustc-link-search=native={}/libs/x86_64/",
-                manifest_dir
-            );
-        }
+    // set up correct linker paths for the according architecture
+    if cfg!(target_arch = "x86") {
+        println!("cargo:rustc-link-search=native={}/libs/x86/", manifest_dir);
+    } else if cfg!(target_arch = "x86_64") {
+        println!(
+            "cargo:rustc-link-search=native={}/libs/x86_64/",
+            manifest_dir
+        );
     }
 
-    // link against this library
-    println!("cargo:rustc-link-lib=PCANBasic");
+    // link against the correct target library
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=PCANBasic");
+    } else if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-lib=dylib=pcanbasic");
+    }
 }
